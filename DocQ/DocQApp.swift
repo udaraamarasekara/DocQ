17b0edadf277570b1dd4759f632ec72fe6907ba3
udara amarasekara <udaraprabath98@gmail.com>
2025-03-23 18:23:00 +0530

    Initial Commit

---
+++ DocQ/DocQApp.swift
@@ -0,0 +1,17 @@
+//
+//  DocQApp.swift
+//  DocQ
+//
+//  Created by udara prabath on 2025-03-23.
+//
+
+import SwiftUI
+
+@main
+struct DocQApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DocQ/UserLoginView.swift
@@ -0,0 +1,18 @@
+//
+//  UserLoginView.swift
+//  DocQ
+//
+//  Created by udara prabath on 2025-03-24.
+//
+
+import SwiftUI
+
+struct UserLoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserLoginView()
+}
